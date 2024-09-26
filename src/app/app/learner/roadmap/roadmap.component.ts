d5b4fe1cf2c228a9fd37e7accd5d232812ee42cd
Mai Minh Đức <maikpla3@gmail.com>
2024-09-26 14:46:31 +0700

    implement base routes

---
+++ src/app/app/learner/roadmap/roadmap.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roadmap',
+  standalone: true,
+  imports: [],
+  templateUrl: './roadmap.component.html',
+  styleUrl: './roadmap.component.scss'
+})
+export class RoadmapComponent {
+
+}
